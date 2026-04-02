392cadab98c1c0fc09c8449a8a37f5568569924c
PavanKalyan99 <danireddypavankalyan@gmail.com>
2026-04-02 09:44:08 +0530

    reverse only letters

---
+++ array-problem-20.py
@@ -0,0 +1,14 @@
+s = "a-bC-dEf-ghIj"
+s=list(s)
+left=0
+right=len(s)-1
+while left<right:
+  if not s[left].isalpha():
+    left+=1
+  elif not s[right].isalpha():
+    right-=1
+  else:
+    s[left], s[right] = s[right], s[left]
+    left+=1
+    right-=1
+print(''.join(s))
